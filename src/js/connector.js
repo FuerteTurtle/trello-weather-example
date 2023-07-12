a904bd13dba53e183dc0d02c95f06f2f91fdf1fe
FuerteTurtle <davegoodrich45@gmail.com>
2023-07-12 10:35:26 -0600

    load card and add card

--- src/js/connector.js
+++ src/js/connector.js
@@ -3,12 +3,9 @@ console.log("Hello World");
 window.TrelloPowerUp.initialize({
   "card-badges": function (t, opts) {
     let cardAttachments = opts.attachments; // Trello passes you the attachments on the card
-    return t
-      .card("name")
-      .get("name")
-      .then(function (cardName) {
-        console.log("We just loaded the card name for fun: " + cardName);
-        return [];
-      });
+    return t.card("all").then(function (card) {
+      console.log(JSON.stringify(card, null, 2));
+      return [{ text: "Dave Broke it" }];
+    });
   },
 });
