789beb926a3684546c6b9f078668c9054e456ea3
FuerteTurtle <davegoodrich45@gmail.com>
2023-07-12 14:26:36 -0600

    badge

--- src/js/connector.js
+++ src/js/connector.js
@@ -4,8 +4,13 @@ window.TrelloPowerUp.initialize({
   "card-badges": function (t, opts) {
     let cardAttachments = opts.attachments; // Trello passes you the attachments on the card
     return t.card("all").then(function (card) {
-      console.log(JSON.stringify(card, null, 2));
-      return [{ text: "Dave Broke it" }];
+      if (card.coordinates) {
+        return [
+          {
+            text: "83 F",
+          },
+        ];
+      }
     });
   },
 });
