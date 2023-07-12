f29c37a743facf741e23429bb50ca89b8bb57342
FuerteTurtle <davegoodrich45@gmail.com>
2023-07-12 10:23:56 -0600

    powerup connections

---
+++ src/js/connector.js
@@ -0,0 +1,14 @@
+console.log("Hello World");
+
+window.TrelloPowerUp.initialize({
+  "card-badges": function (t, opts) {
+    let cardAttachments = opts.attachments; // Trello passes you the attachments on the card
+    return t
+      .card("name")
+      .get("name")
+      .then(function (cardName) {
+        console.log("We just loaded the card name for fun: " + cardName);
+        return [];
+      });
+  },
+});
