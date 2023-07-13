103dc3d74e35916828dbbfb43e2ea6ecab49ddd6
FuerteTurtle <davegoodrich45@gmail.com>
2023-07-13 13:32:26 -0600

    add html popup and button

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,16 +1,17 @@
 console.log("Hello World");
 
 window.TrelloPowerUp.initialize({
-  "card-badges": function (t, opts) {
-    let cardAttachments = opts.attachments; // Trello passes you the attachments on the card
-    return t.card("all").then(function (card) {
-      if (card.coordinates) {
-        return [
-          {
-            text: "83 F",
-          },
-        ];
-      }
-    });
+  "card-buttons": function (t, options) {
+    return [
+      {
+        text: "Open PR",
+        callback: function (t) {
+          return t.popupt({
+            title: "PR",
+            url: "pr.html",
+          });
+        },
+      },
+    ];
   },
 });
