3b8cd73c1775d002e5e18941b366d849ff78f1cd
FuerteTurtle <davegoodrich45@gmail.com>
2023-07-14 08:37:08 -0600

    parse card info from url

--- src/js/create.js
+++ src/js/create.js
@@ -1,6 +1,22 @@
+const linkRegex = /^https:\/\/github\.com\/[^\/]+\/([^\/]+)\/pull\/(\d+)$/;
+function parseGitHubURL(url) {
+  const match = url.match(linkRegex);
+  if (match) {
+    const repository = match[1];
+    const pullRequestNumber = match[2];
+    console.log({ repository, pullRequestNumber });
+    return {
+      repository,
+      pullRequestNumber,
+      details: `[pr ${pullRequestNumber}](${url})`,
+    };
+  }
+  return undefined;
+}
+
 let trello = TrelloPowerUp.iframe();
 
 window.addEventListener("submit", function (event) {
   event.preventDefault();
-  console.log(document.getElementById("github-link").value);
+  parseGitHubURL(document.getElementById("github-link").value);
 });
